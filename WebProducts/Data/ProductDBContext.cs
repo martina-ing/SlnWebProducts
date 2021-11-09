914d51796a3d08b533264a3e6877431db6abbedf
Martina <m.lusardi@hotmail.com>
2021-11-08 22:42:13 -0300

    Add project files.

---
+++ WebProducts/Data/ProductDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebProducts.Models;
+
+namespace WebProducts.Data
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext() : base("KeyProductDB") { }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ WebProducts/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebProducts.Models
+{
+    public class Product
+    {
+        [Key()]
+        public int Id { get; set; }
+
+        public string Category { get; set; }
+
+        [DisplayName("Name")]
+
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+
+        [DisplayName("Available Date")]
+        [DataType(DataType.Date)]
+        public DateTime AvailableDate { get; set; }
+
+
+    }
+}
